61a43641e52f15167251538bbb5d6a6769f87a7c
Audby <Audun.ystgaard@gmail.com>
2024-01-11 15:01:09 +0100

    Oving1
    Oving1

---
+++ Oving1/main.cpp
@@ -0,0 +1,19 @@
+//Oppgave 1:
+
+/*
+Oppgave 1a) Kode til python:
+
+def isFibonacciNumber(n):
+    a = 0
+    b = 1
+    while b < n:
+        temp = b
+        b += a
+        a = temp
+    
+    return b
+
+*/
+
+//Oppgave 2:
+
